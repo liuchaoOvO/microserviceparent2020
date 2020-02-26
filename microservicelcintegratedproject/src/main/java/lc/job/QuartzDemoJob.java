4cd999358c0886a7fd799bcf8dfe662181c43a71
liuchaoy <liuchaoy@yonyou.com>
2020-02-26 16:40:14 +0800

    优化代码

--- microservicelcintegratedproject/src/main/java/lc/job/QuartzDemoJob.java
+++ microservicelcintegratedproject/src/main/java/lc/job/QuartzDemoJob.java
@@ -7,20 +7,16 @@ import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 /**
  * @author liuchaoOvO on 2019/4/18
  */
-public class QuartzJobFactory implements Job
-{
-    private static final Logger logger = LoggerFactory.getLogger(MultiResolverTypeContrller.class);
+public class QuartzDemoJob implements Job {
+    private static final Logger logger = LoggerFactory.getLogger(QuartzDemoJob.class);
 
     @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException
-    {
+    public void execute(JobExecutionContext context) throws JobExecutionException {
         //TODO 这里是定时任务逻辑
-        logger.info("=================我是定时任务,每隔5秒执行一次==============");
+        logger.info("=================我是测试定时任务,设置为每隔5秒执行一次==============");
     }
-
 
 }
